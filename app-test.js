7dc3b08272755bac1d424537df1bacb47066daa8
sabyag42 <106244612+sabyag42@users.noreply.github.com>
2026-02-10 16:58:56 +0530

    fail test case

--- app-test.js
+++ app-test.js
@@ -51,7 +51,7 @@ describe('Planets API Suite', () => {
               .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.have.property('id').eql(3);
-                    res.body.should.have.property('name').eql('Earth');
+                    res.body.should.have.property('name').eql('Earth-error');
                 done();
               });
         });
